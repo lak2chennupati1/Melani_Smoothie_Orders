76d015449f26aeee3f0ba5c702c5f00405bce6ac
Laxmi Narayana <lak.chennupati@gmail.com>
2026-05-25 23:31:03 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+# Import python packages
+import streamlit as st
+import requests
+
+# Write directly to the app
+st.title(':cup_with_straw: Pending Smoothie Orders :cup_with_straw:')
+st.write(
+  """Orders that needs to be filled!
+  """)
+cnx = st.connetion('snowflake')
+session = cnx.session()
+session = get_active_session()
+
+my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
+if my_dataframe:
+    editable_df= st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try: 
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+            st.success("Someone clicked the button.", icon='👍')
+        except:
+            st.write('Something went wrong')
+else:
+    st.success('There are no pending orders right now', icon='👍')
